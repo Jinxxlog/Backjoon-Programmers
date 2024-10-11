bda33a82b5f7f0d6a415a4c4ebe0ab0913566f5b
Jinhyung Kim <mir001125@naver.com>
2024-10-11 15:12:50 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 109.23 ms, Memory: 27.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -0,0 +1,10 @@
+def solution(arr):
+    a = []
+    
+    a.append(arr[0])
+    
+    for i in range(1, len(arr)):
+        if arr[i] != arr[i-1]:
+            a.append(arr[i])
+        
+    return a
