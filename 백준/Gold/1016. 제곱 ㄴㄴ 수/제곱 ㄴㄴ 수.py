70bca25dafaff896a7b960e4fa2120b996abe9f9
Jinhyung Kim <mir001125@naver.com>
2024-10-13 16:51:00 +0900

    [Gold I] Title: 제곱 ㄴㄴ 수, Time: 844 ms, Memory: 38932 KB -BaekjoonHub

--- 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.py
+++ 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.py
@@ -1,12 +1,12 @@
 n, m = map(int, input().split())
 
-res = [True] * (m-n+1)
+res = [1] * (m-n+1)
 
 for i in range(2, int(m ** 0.5)+1):
     a = i ** 2
-    st = max(a, (n+a-1) // a*a)
+    st = max(a, ((n + a - 1) // a) * a)
     
     for j in range(st, m+1, a):
-        res[j-n] = False
+        res[j-n] = 0
 
-print(sum(res))
+print(res.count(1))
